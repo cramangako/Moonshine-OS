9af0e72941e298b44af8fedb52f5108c2944526e
Losercities <133237995+Warrior1711@users.noreply.github.com>
2026-02-18 09:10:19 +0000

    Update kernel.cpp
    Add more stuff

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -25,6 +25,13 @@ extern "C" void kernel_main(uint32_t magic, uint32_t* mbi_ptr) {
     set_color(15, 0);
     print("kernel loaded. peak has finally arrived\n");
 
+set_color(12, 0);
+    print("Made By Realyby And Cramangako\n");
+
+set_color(9, 0);
+    print("Check the Cmds.MD for help on commands!\n");
+
+
     while (true) { // halt forever!!!
         asm volatile("hlt");
     }
