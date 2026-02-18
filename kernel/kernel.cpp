88c65ea5feee4d890e372baeb8641e9b28e04ca4
cramangako <wljroccio@glow.sch.uk>
2026-02-18 09:20:43 +0000

    Replace print statements with speak function

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -8,29 +8,29 @@ extern "C" void kernel_main(uint32_t magic, uint32_t* mbi_ptr) {
 
     if (magic != 0x36D76289) { // this has to happen im just the messenger
         set_color(12, 0);
-        print("not booted by multiboot2!\n");
+        speak("not booted by multiboot2!\n");
         return;
     }
 
     for (int n = 0; n < 200; n++) {
         set_color(n, (n^67) % 16);
-        print("real loading screen.....\n");
+        speak("real loading screen.....\n");
     }
 
     set_color(0, 0);
     clear();
 
     set_color(10, 0); // black background
-    print("Moonshine-OS v2.0.1\n");
+    speak("Moonshine-OS v2.0.1\n");
 
     set_color(15, 0);
-    print("kernel loaded. peak has finally arrived\n");
+    speak("kernel loaded. peak has finally arrived\n");
 
     set_color(12, 0);
-    print("Made By Realyby And Cramangako\n");
+    speak("Made By Realyby And Cramangako\n");
 
     set_color(9, 0);
-    print("Check the Cmds.MD for help on commands!\n");
+    speak("Check the Cmds.MD for help on commands!\n");
 
 
     while (true) { // halt forever!!!
